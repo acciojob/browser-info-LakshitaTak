e6e5b25e12f238fed9bc095f5e92a9c4c89a0ada
LakshitaTak <134226199+LakshitaTak@users.noreply.github.com>
2023-07-20 21:38:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your JS code here. If required.
 console.log(navigator)
+let x= document.getElementById('browser-info')
+console.log(x)
+
+let y= `You are using ${navigator.appName} version ${navigator.appVersion}`
+console.log(y)
+
+x.innerHTML = y
